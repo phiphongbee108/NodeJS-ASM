55f1de406462f90fb7a257db02d9fbe0b81e9863
TamTD <phiphongbee108@gmail.com>
2022-12-19 16:53:32 +0700

    BE add user

---
+++ ASM2-BE/app.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("639d82bdefaed60ff5954146")
+    .then((user) => {
+      req.user = new User(
+        user.username,
+        user.password,
+        user.fullName,
+        user.phoneNumber,
+        user.email,
+        user.isAdmin,
+        user._id
+      );
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
